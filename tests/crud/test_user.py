77dc94453d50de9c1ca80b2b6303586165336420
sadegh <sadegh.moayedizadeh@gmail.com>
2022-11-26 17:58:15 +0330

    Test disabled user should be active.

--- tests/crud/test_user.py
+++ tests/crud/test_user.py
@@ -62,13 +62,18 @@ def test_check_if_user_is_active(
     assert is_active is True
 
 
-# def test_check_if_user_is_active_inactive(db: Session) -> None:
-#     email = random_email()
-#     password = random_lower_string()
-#     user_in = UserCreate(email=email, password=password, disabled=True)
-#     user = user_crud.create(db, obj_in=user_in)
-#     is_active = user_crud.is_active(user)
-#     assert is_active
+def test_disabled_user_should_be_active(
+    db: Session,
+    fake_email: str,
+    fake_password: str,
+    delete_user_by_email: None
+) -> None:
+    email = fake_email
+    password = fake_password
+    user_in = UserCreate(email=email, password=password, disabled=True)
+    user = user_crud.create(db, obj_in=user_in)
+    is_active = user_crud.is_active(user)
+    assert is_active
 
 
 # def test_check_if_user_is_superuser(db: Session) -> None:
