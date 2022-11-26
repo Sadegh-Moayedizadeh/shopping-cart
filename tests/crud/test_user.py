a9251162c420f6cd86f5eb583725fae242616125
sadegh <sadegh.moayedizadeh@gmail.com>
2022-11-26 18:03:26 +0330

    Test update user.

--- tests/crud/test_user.py
+++ tests/crud/test_user.py
@@ -2,7 +2,7 @@ from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from shopping_cart.crud import user_crud
-# from app.core.security import verify_password
+from shopping_cart.utils.security import verify_password
 from shopping_cart.schemas import UserCreate, UserUpdate
 from shopping_cart.models import User
 
@@ -114,15 +114,18 @@ def test_get_user(
     assert jsonable_encoder(user) == jsonable_encoder(user_2)
 
 
-# def test_update_user(db: Session) -> None:
-#     password = random_lower_string()
-#     email = random_email()
-#     user_in = UserCreate(email=email, password=password, is_superuser=True)
-#     user = user_crud.create(db, obj_in=user_in)
-#     new_password = random_lower_string()
-#     user_in_update = UserUpdate(password=new_password, is_superuser=True)
-#     user_crud.update(db, db_obj=user, obj_in=user_in_update)
-#     user_2 = user_crud.get(db, id=user.id)
-#     assert user_2
-#     assert user.email == user_2.email
-#     assert verify_password(new_password, user_2.hashed_password)
+def test_update_user(
+    db: Session,
+    fake_email: str,
+    fake_password: str,
+    delete_user_by_email: None
+) -> None:
+    user_in = UserCreate(email=fake_email, password=fake_password, is_superuser=True)
+    user = user_crud.create(db, obj_in=user_in)
+    new_password = 'new_p@ssword'
+    user_in_update = UserUpdate(password=new_password, is_superuser=True)
+    user_crud.update(db, db_obj=user, obj_in=user_in_update)
+    user_2 = user_crud.get(db, id=user.id)
+    assert user_2
+    assert user.email == user_2.email
+    assert verify_password(new_password, user_2.hashed_password)
