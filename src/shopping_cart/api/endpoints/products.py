fe623c689e954c6e93edbac4d59c21fab8be6be6
sadegh <sadegh.moayedizadeh@gmail.com>
2022-11-29 17:06:38 +0330

    Design and test plan.

---
+++ src/shopping_cart/api/endpoints/products.py
@@ -0,0 +1,6 @@
+# View Product
+# View all products (see how many they are. think of something like pagination)
+# Add product
+# Remove product
+# Show user's selected products
+# Purchase the selected products
---
+++ tests/api/test_products.py
@@ -0,0 +1,22 @@
+def test_add_product_should_update_users_product_ids() -> None:
+    pass
+
+
+def test_add_product_id_that_does_not_exist_should_raise_error() -> None:
+    pass
+
+
+def test_remove_product_should_remove_its_id_from_users_product_ids() -> None:
+    pass
+
+
+def test_remove_product_that_is_not_present_in_users_product_ids_should_raise_error() -> None:  # noqa: E501
+    pass
+
+
+def test_show_users_selected_products_should_return_all_its_product_ids() -> None:  # noqa: E501
+    pass
+
+
+def test_purchase_selected_products_should_empty_users_product_ids() -> None:
+    pass
